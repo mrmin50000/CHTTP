f83eb508ac534cc075cead28b4bf280e239e4c21
mrmin50000 <danilast2007@gmail.com>
2025-08-04 13:43:29 +0300

    TCP

--- http.c
+++ http.c
@@ -7,4 +7,34 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define PORT 8080 
+#define BACKLOG 10
 
+int main() {
+    int server_fd;
+    struct sockaddr_in server_addr;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Error of creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error of connection");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(server_fd, BACKLOG) < 0) {
+        perror("Error listen()");
+        exit(EXIT_FAILURE);
+    }    
+
+    printf("Server works on port: %d\n", PORT);
+    close(server_fd);
+    return 0;
+}
